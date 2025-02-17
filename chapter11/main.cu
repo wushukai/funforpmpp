8013f1b74fd0a83019c6445fec2bacf8116880fd
WUSHUKAI <wushukainju@gmail.com>
2025-02-17 14:12:12 +0800

    1

--- chapter11/main.cu
+++ chapter11/main.cu
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include <cuda_runtime.h>
 
 
 __global__
-void prefix_sum_kernel(const float32_t *data, float32_t *result, int n) {
-    __shared__ float32_t accm_buffer1[32];
-    __shared__ float32_t accm_buffer2[32];
+void prefix_sum_kernel(const float *data, float *result, int n) {
+    __shared__ float accm_buffer1[32];
+    __shared__ float accm_buffer2[32];
 
     int tid = threadIdx.x;
     if (tid < n) {
         accm_buffer1[tid] = data[tid];
     }
 
-    float32_t *read_buffer = accm_buffer1;
-    float32_t *write_buffer = accm_buffer2;
+    float *read_buffer = accm_buffer1;
+    float *write_buffer = accm_buffer2;
 
     for (int stride = 1; stride < n; stride *= 2) {
         __syncthreads();
@@ -35,12 +36,12 @@ void prefix_sum_kernel(const float32_t *data, float32_t *result, int n) {
     result[tid] = read_buffer[tid];
 }
 
-int calc_prefix_sum(const float32_t *data, float32_t *result, int n) {
-    float32_t *d_data, *d_result;
-    cudaMalloc(&d_data, n * sizeof(float32_t));
-    cudaMalloc(&d_result, n * sizeof(float32_t));
+int calc_prefix_sum(const float *data, float *result, int n) {
+    float *d_data, *d_result;
+    cudaMalloc(&d_data, n * sizeof(float));
+    cudaMalloc(&d_result, n * sizeof(float));
 
-    cudaMemcpy(d_data, data, n * sizeof(float32_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_data, data, n * sizeof(float), cudaMemcpyHostToDevice);
 
     // call kernel
     prefix_sum_kernel<<<1, 32>>>(d_data, d_result, n);
@@ -52,7 +53,7 @@ int calc_prefix_sum(const float32_t *data, float32_t *result, int n) {
         exit(EXIT_FAILURE);
     }
 
-    err = cudaMemcpy(d_result, result, n * sizeof(float32_t), cudaMemcpyDeviceToHost);
+    err = cudaMemcpy(d_result, result, n * sizeof(float), cudaMemcpyDeviceToHost);
     if (err != cudaSuccess) {
         printf("Failed to copy data from device to host for d_result: %s\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
@@ -63,11 +64,11 @@ int calc_prefix_sum(const float32_t *data, float32_t *result, int n) {
 }
 
 int main() {
-    const float32_t data[] = [
+    const float data[] = {
         4, 6, 7, 1, 2, 8, 5, 2
-    ];
+    };
 
-    float32_t result[sizeof(data)/sizeof(data[0])];
+    float result[sizeof(data)/sizeof(data[0])];
 
     int r = calc_prefix_sum(data, result, sizeof(data)/sizeof(data[0]));
     for (int i = 0; i < sizeof(data)/sizeof(data[0]); i++) {
